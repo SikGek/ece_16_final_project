0b875fb148d9f4174ce290fc3092de5aee5b9851
SikGek <81644416+SikGek@users.noreply.github.com>
2022-06-08 01:03:06 -0700

    asd

--- ece_16_final.py
+++ ece_16_final.py
@@ -93,7 +93,6 @@ if __name__ == "__main__":
                         break
                 cap.release()
                 cv2.destroyAllWindows()
-                break
 
     except(Exception, KeyboardInterrupt) as e:
         print(e)                     # Exiting the program due to exception
